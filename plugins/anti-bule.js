a949294b23081cfe08bf5694c206b6f4084c7382
AyGemuy <91945119+AyGemuy@users.noreply.github.com>
2022-09-28 00:48:49 +0800

    New
    New

--- plugins/anti-bule.js
+++ plugins/anti-bule.js
@@ -7,20 +7,24 @@ let user = global.db.data.users[m.sender]
    if (m.sender.startsWith('212' || '212')) {
    	user.banned = true
    	this.sendButton(m.chat, caption, author, null, [['Ok', 'Huuu']], m, { mentions: this.parseMention(caption) })
+   	return conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
    }
    
    if (m.sender.startsWith('265' || '265')) {
    	user.banned = true
    	this.sendButton(m.chat, caption, author, null, [['Ok', 'Huuu']], m, { mentions: this.parseMention(caption) })
-   } 
+   	return conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
+   }
    
    if (m.sender.startsWith('92' || '92')) {
    	user.banned = true
    	this.sendButton(m.chat, caption, author, null, [['Ok', 'Huuu']], m, { mentions: this.parseMention(caption) })
-   } 
+   	return conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
+   }
    
    if (m.sender.startsWith('90' || '90')) {
    	user.banned = true
    	this.sendButton(m.chat, caption, author, null, [['Ok', 'Huuu']], m, { mentions: this.parseMention(caption) })
-   } 
+   	return conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
+   }
   }
